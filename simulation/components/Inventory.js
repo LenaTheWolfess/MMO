62d2b27583eb28477f9209cfc5b1a327aa2d38a6
Angen <slavomir.slovenkai@gmail.com>
2020-08-26 09:59:29 +0200

    Differentiate between bag and equipment

--- simulation/components/Inventory.js
+++ simulation/components/Inventory.js
@@ -7,37 +7,89 @@ Inventory.prototype.Schema =
 
 Inventory.prototype.Init = function() {
 	this.items = [];
+	this.bag = [];
+	this.capacity = 24;
 }
 
-Inventory.prototype.Use = function(id) {
+Inventory.prototype.UseItem = function(id) {
 	const cmpItem = Engine.QueryInterface(id, IID_Item);
 	if (!cmpItem)
 		return;
 	cmpItem.Apply(this.entity);
-	if (cmpItem.ShouldBeDestroyed())
+	if (cmpItem.ShouldBeDestroyed()) {
 		Engine.DestroyEntity(id);
+		const indx = this.bag.indexOf(id);
+		if (indx != -1)
+			this.bag[indx] = undefined;
+	}
 }
 
 Inventory.prototype.Add = function(id) {
-	const cmpItem = Engine.QueryInterface(id, IID_Equipment);
-	if (!cmpItem) {
-		this.Use(id);
+	let hasFreeRoom = this.HasFreeRoom();
+	const cmpEquipment = Engine.QueryInterface(id, IID_Equipment);
+	if (!cmpEquipment) {
+		const cmpItem = Engine.QueryInterface(id, IID_Item);
+		if (!hasFreeRoom || !cmpItem.CanBeStored())
+			this.UseItem(id);
+		else
+			this.Take(id);
 		return;
 	}
-	const type = cmpItem.GetType();
-	const specific = cmpItem.GetTypeSpecific();
-	const anim = cmpItem.GetAnimation();
-	this.Drop(type);
-	this.RemoveFrom(type);
-	this.AddTo(id, type, specific, anim);
+	const type = cmpEquipment.GetType();
+	const haveToRemove = !cmpEquipment.CanBeStored() && this.items[type];
+	if (haveToRemove) {
+		const idx = this.UnEquip(type);
+		if (idx) {
+			const cmpEquipmentX = Engine.QueryInterface(id, IID_Equipment);
+			if (!hasFreeRoom || !cmpEquipmentX.CanBeStored()) {
+				this.Drop(idx);
+				hasFreeRoom = this.HasFreeRoom();
+			}
+		}
+	}
+	if (!hasFreeRoom) {
+		warn("no place in inventory, nor can switch equipment");
+		return;
+	}
 	this.Take(id);
+	if (!this.items[type])
+		this.Equip(id, type);
 }
 
-Inventory.prototype.AddTo = function(id, type, specific, anim) {
+Inventory.prototype.Use = function(id) {
+	if (!id)
+		return;
+	const cmpEquipment = Engine.QueryInterface(id, IID_Equipment);
+	if (cmpEquipment) {
+		this.SwitchEquipment(id, cmpEquipment.GetType());
+		return;
+	}
+	this.UseItem(id);
+}
+
+Inventory.prototype.SwitchEquipment = function(id, type) {
+	if (this.items[type] && this.items[type] != id) {
+		const idx = this.UnEquip(type);
+		const cmpEquipmentX = Engine.QueryInterface(id, IID_Equipment);
+		if (!cmpEquipmentX.CanBeStored())
+			this.Drop(idx);
+	}
+	this.Equip(id, type);
+}
+
+Inventory.prototype.Equip = function(id, type) {
+	if (this.items[type])
+		return;
+	const cmpEquipment = Engine.QueryInterface(id, IID_Equipment);
+	if (!cmpEquipment)
+		return;
 	this.items[type] = id;
+	this.AddBonus(id);
 	const cmpVisual = Engine.QueryInterface(this.entity, IID_Visual);
 	if (!cmpVisual)
 		return;
+	const anim = cmpEquipment.GetAnimation();
+	const specific = cmpEquipment.GetTypeSpecific();
 	cmpVisual.SetVariant(type, anim);
 	if (type == "weapon") {
 		const n = "animations-"+specific+"-combat";
@@ -71,7 +123,7 @@ Inventory.prototype.AddBonus = function(id) {
 		cmpAura.ApplyFormationAura([this.entity]);
 }
 
-Inventory.prototype.RemoveFrom = function(type) {
+Inventory.prototype.UnEquip = function(type) {
 	if (!type)
 		return;
 	const id = this.items[type];
@@ -81,7 +133,7 @@ Inventory.prototype.RemoveFrom = function(type) {
 	this.items[type] = undefined;
 	const cmpVisual = Engine.QueryInterface(this.entity, IID_Visual);
 	if (!cmpVisual)
-		return;
+		return id;
 	// Item variant to show
 	cmpVisual.SetVariant(type, type+"-none");
 	if (type == "weapon") {
@@ -90,22 +142,27 @@ Inventory.prototype.RemoveFrom = function(type) {
 			cmpVisual.SetVariant("cape", "cape-none");
 		}
 	}
+	return id;
 }
 
 Inventory.prototype.Take = function(id) {
 	if (!id)
 		return;
-	this.AddBonus(id);
+	if (this.bag.indexOf(id) != -1)
+		return;
+	this.bag.push(id);
 	const cmpPosition = Engine.QueryInterface(id, IID_Position);
 	if (!cmpPosition)
 		return;
 	cmpPosition.MoveOutOfWorld();
 }
 
-Inventory.prototype.Drop = function(type) {
-	const id = this.items[type];
+Inventory.prototype.Drop = function(id) {
 	if (!id)
 		return;
+	const indx = this.bag.indexOf(id);
+	if (indx != -1)
+		this.bag[indx] = undefined;
 	const cmpPosition = Engine.QueryInterface(id, IID_Position);
 	if (!cmpPosition)
 		return;
@@ -123,6 +180,18 @@ Inventory.prototype.GetItems = function() {
 		result.push({"id": this.items[type]});
 	}
 	return result;
+}
+
+Inventory.prototype.GetBag = function() {
+	let result = [];
+	for (let x in this.bag) {
+		result.push({"id": this.bag[x]});
+	}
+	return result;
+}
+
+Inventory.prototype.HasFreeRoom = function() {
+	return this.bag.length < this.capacity;
 }
 
 Engine.RegisterComponentType(IID_Inventory, "Inventory", Inventory);
