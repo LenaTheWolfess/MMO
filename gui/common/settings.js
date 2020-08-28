0e9e91573700ed50def67a626ae0c449cff94a97
Angen <slavomir.slovenkai@gmail.com>
2020-08-28 15:30:39 +0200

    test if 16 players are supported

--- gui/common/settings.js
+++ gui/common/settings.js
@@ -2,7 +2,7 @@
  * The maximum number of players that the engine supports.
  * TODO: Maybe we can support more than 8 players sometime.
  */
-const g_MaxPlayers = 8;
+const g_MaxPlayers = 15;
 
 /**
  * The maximum number of teams allowed.
