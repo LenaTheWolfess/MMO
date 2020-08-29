aae18b6c5c2efdb028b463e9b5d864a3740596a4
SlavomirSlovenkai <slavomir.slovenkai@gmail.com>
2020-08-29 13:23:27 +0200

    multiplayer dungeon and init InitGame.js

--- maps/scripts/Boss.js
+++ maps/scripts/Boss.js
@@ -1,6 +1,5 @@
 Trigger.prototype.InitBossGame = function(msg)
 {
-	this.bosses = [];
 	const cmpRangeManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager);
 	for (let playerID = 0; playerID < TriggerHelper.GetNumberOfPlayers(); ++playerID) {
 		const ents = cmpRangeManager.GetEntitiesByPlayer(playerID);
@@ -37,7 +36,7 @@ Trigger.prototype.CheckBossDefeat = function(data)
 
 {
 	let cmpTrigger = Engine.QueryInterface(SYSTEM_ENTITY, IID_Trigger);
-	cmpTrigger.regicideHeroes = [];
+	cmpTrigger.bosses = [];
 	cmpTrigger.DoAfterDelay(0, "InitBossGame", {});
 	cmpTrigger.RegisterTrigger("OnOwnershipChanged", "CheckBossDefeat", { "enabled": true });
 	cmpTrigger.RegisterTrigger("OnEntityRenamed", "RenameBoss", { "enabled": true });
